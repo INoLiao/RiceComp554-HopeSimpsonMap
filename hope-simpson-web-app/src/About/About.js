30f5837bdebb14a25fcccdab8965f67a984f7927
howardwangtech <59592934+howardwangtech@users.noreply.github.com>
2020-11-18 20:08:36 -0600

    added slides to about page

--- hope-simpson-web-app/src/About/About.js
+++ hope-simpson-web-app/src/About/About.js
@@ -1,13 +1,82 @@
 import React from "react";
 import './About.css';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const slideImages = [
+  './images/p0.jpg',
+  './images/p1.jpg',
+  './images/p2.jpg',
+  './images/p3.jpg',
+  './images/p4.jpg',
+  './images/p5.jpg',
+  './images/p6.jpg',
+  './images/p7.jpg',
+  './images/p8.jpg'
+];
+
+const properties = {
+  duration: 22000,
+  transitionDuration: 600,
+  infinite: true,
+  indicators: true,
+  arrows: true
+}
+
+
+const Slideshow = () => {
+    return (
+      <div>
+        <Slide easing="ease" {...properties}>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}> 
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[3]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[4]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[5]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[6]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[7]})`}}>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[8]})`}}>
+            </div>
+          </div>
+        </Slide>
+      </div>
+    )
+};
 
 function About() {
   return (
     <div className="about-container container">
       <div className="about-kanban rounded">
-        <div className="display-4">About Page</div>
-        <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at commodi dolore iure minus perferendis perspiciatis quae, quaerat repellendus voluptatibus.</p>
+        <div className="display-4">About the Hope-Simpson Concept</div>
+        <p className="lead"></p>
       </div>
+	{Slideshow()}
     </div>
   );
 }
