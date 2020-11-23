b32d88499abfc7e3aeda17bdc67068b761cd3cdd
Charlie Shih <cyshih73@gmail.com>
2020-11-23 16:57:37 -0600

    New GDS View Remove console message Tab in a map functions

--- hope-simpson-web-app/src/Map/MapGDS.js
+++ hope-simpson-web-app/src/Map/MapGDS.js
@@ -4,8 +4,13 @@ import './Map.css';
 const GDS_REPORT_PATHS = [
   // Overview
   "https://datastudio.google.com/embed/reporting/30c1e5a8-6cbf-43ba-9796-b4bb042082f6/page/TxgjB",
-  // Four Zones
-  "https://datastudio.google.com/embed/reporting/30c1e5a8-6cbf-43ba-9796-b4bb042082f6/page/cm8pB"
+  // Four Zones Concept
+  "https://datastudio.google.com/embed/reporting/30c1e5a8-6cbf-43ba-9796-b4bb042082f6/page/cm8pB",
+  // Province/State Slice
+  "https://datastudio.google.com/embed/reporting/30c1e5a8-6cbf-43ba-9796-b4bb042082f6/page/dF9pB"
+]
+const GDS_REPORT_TITLES = [
+  "Overview", "Four Zones Concept", "Province/State Slice"
 ]
 
 class MapGDS extends React.Component{
@@ -20,26 +25,25 @@ class MapGDS extends React.Component{
   }
 
   render() {
-    console.log(this.state);
     return (
       <div className="map-container container align-items-center">
         <ul className="nav nav-tabs justify-content-end">
           <li className="nav-item disabled">
             <a className="nav-link disabled" href="/#">COVID-19 World Map <span style={{fontSize: "xx-small"}}>Powered by Google Data Studio</span></a>
           </li>
-          <li className="nav-item">
-            <a className={ "nav-link " + (this.state.view === 0 ? "active" : "") } 
-              onClick={ (e) => this.handleChangeView(0, e) } href="/#">Overview</a>
-          </li>
-          <li className="nav-item">
-            <a className={ "nav-link " + (this.state.view === 1 ? "active" : "") } 
-              onClick={ (e) => this.handleChangeView(1, e) } href="/#">Four Zones Concept</a>
-          </li>
+          {
+            GDS_REPORT_TITLES.map((title, idx) => {
+              return <li className="nav-item" key={ `Tab_${idx}` }>
+                <a className={ "nav-link " + (this.state.view === idx ? "active" : "") } 
+                  onClick={ (e) => this.handleChangeView(idx, e) } href="/#">{title}</a>
+              </li>
+            })
+          }
         </ul>
         { 
           GDS_REPORT_PATHS.map((report, idx) => {
             return <iframe width="1110px" height="910px" frameBorder="0" title="COVID-19 Map with GDS" 
-            key={ idx } src={ report }
+            key={ `Iframe_${idx}` } src={ report }
             allowFullScreen style={this.state.view === idx ? {position: "absolute"} : { position: "absolute", top: "-5000px" }}/>
           })
         }
