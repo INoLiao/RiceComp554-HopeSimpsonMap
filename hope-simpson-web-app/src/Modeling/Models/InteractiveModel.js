272b4a1c3b56f6c195cf11a0cd5b933f22d932b5
ViniBR01 <vinicius@rice.edu>
2020-12-07 13:28:15 -0600

    created static display of a seasonal model

--- hope-simpson-web-app/src/Modeling/Models/InteractiveModel.js
+++ hope-simpson-web-app/src/Modeling/Models/InteractiveModel.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
+
+
 //function InteractiveModel() {
 class InteractiveModel extends Component {
     constructor(props) {
@@ -16,9 +18,22 @@ class InteractiveModel extends Component {
 	this.displayChart();
     }
 
+    generateAlpha(base_alpha, seasonal_factor, total_time) {
+	let alpha = [];
+	for (let j=0; j < total_time - 1; j++) {
+	    let val = base_alpha*(1 + seasonal_factor*Math.sin(j/365*2*Math.PI));
+	    alpha.push(val);
+	}
+	//console.log(alpha);
+	return alpha;
+    }
+    
     generateData(param, initialValues) {
-	const total_time = 3500;
+	const total_time = 1500;
 	let current_date = new Date('2020-02-02');
+
+	const seasonal_factor = 0.75;
+	const alpha = this.generateAlpha(param.alpha, seasonal_factor, total_time);
 	
 	let s = initialValues.S;
 	let s_next;
@@ -41,8 +56,8 @@ class InteractiveModel extends Component {
 
 	for (let j=0; j < total_time - 1; j++) {
 	    s_next = s - param.beta*i*s + param.etha*r;
-	    e_next = e + param.beta*i*s - param.alpha*e;
-	    i_next = i + param.alpha*e - param.ro*i;
+	    e_next = e + param.beta*i*s - alpha[j]*e;
+	    i_next = i + alpha[j]*e - param.ro*i;
 	    r_next = r + param.ro*i - param.etha*r;
 	    s = s_next;
 	    e = e_next;
@@ -68,7 +83,7 @@ class InteractiveModel extends Component {
 	//Define general dimension parameters
 	var outerWidth = 800;
 	var outerHeight = 550;
-	var margin = { left: 70, top: 5, right: 5, bottom: 60 };
+	var margin = { left: 70, top: 25, right: 25, bottom: 60 };
 	var innerWidth  = outerWidth  - margin.left - margin.right;
 	var innerHeight = outerHeight - margin.top  - margin.bottom;
 	var xColumn = "timestamp";
@@ -90,24 +105,31 @@ class InteractiveModel extends Component {
 	var xAxisG = g.append("g")
 	    .attr("class", "x axis")
 	    .attr("transform", "translate(0, " + innerHeight + ")");
-	var xAxisLabel = xAxisG.append("text")
+	/*var xAxisLabel = xAxisG.append("text")
 	    .style("text-anchor", "middle")
-            .attr("transform", "translate(" + (innerWidth / 2) + "," + xAxisLabelOffset + ")")
+            .attr("transform",
+		  "translate(" + (innerWidth / 2) + "," + xAxisLabelOffset + ")")
             .attr("class", "label")
-            .text(xAxisLabelText);
+            .text(xAxisLabelText);*/
 	var yAxisG = g.append("g")
             .attr("class", "y axis");
-	var yAxisLabel = yAxisG.append("text")
+	/*var yAxisLabel = yAxisG.append("text")
             .style("text-anchor", "middle")
-            .attr("transform", "translate(-" + yAxisLabelOffset + "," + (innerHeight / 2) + ") rotate(-90)")
+            .attr("transform",
+		  "translate(-" + yAxisLabelOffset + "," + (innerHeight / 2) + ") rotate(-90)")
             .attr("class", "label")
-            .text(yAxisLabelText);
+            .text(yAxisLabelText);*/
 
 	//Prepare function to scale input to pixel values
 	var xScale = d3.scaleTime().range([0, innerWidth]);
 	var yScale = d3.scaleLinear().range([innerHeight, 0]);
-	xScale.domain( d3.extent(data, function (d){ return d[xColumn]; }));
-	yScale.domain( d3.extent(data, function (d){ return d[yColumn]; }));
+	var minDate = d3.min(data, function (d){ return d[xColumn]; });
+	minDate.setDate(minDate.getDate() - 1);
+	var maxDate = d3.max(data, function (d){ return d[xColumn]; });
+	maxDate.setDate(maxDate.getDate()); //fix-me: set to Jan 1st of next year
+	//console.log([minDate, maxDate]);
+	xScale.domain( [minDate, maxDate]);
+	yScale.domain( [0, d3.max(data, function (d){ return d[yColumn]; }) * 1.15]);
 
 	//Draw Line
 	const line = d3.line()
@@ -128,7 +150,7 @@ class InteractiveModel extends Component {
     }
     
     displayChart() {
-	const parameters = {
+	var parameters = {
 	    beta: 0.1,
 	    alpha: 0.015,
 	    ro: 0.05,
@@ -151,9 +173,14 @@ class InteractiveModel extends Component {
 	return (
 	    <div>
 		<div>
-		    <p>Ratio of Infected People Over Time</p>
+		    <h3>Ratio of Infected People Over Time</h3>
 		</div>
 		<div ref={this.myChart}></div>
+		<div>
+		    <h4>Control the model parameters below:</h4>
+		    
+		</div>
+		
 	    </div>
 	);
     }
