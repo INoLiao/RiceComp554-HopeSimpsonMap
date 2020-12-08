d0a844221c1391aee584e1ea18b5a632e5ac57ff
DingyuWangCS <59592934+howardwangtech@users.noreply.github.com>
2020-12-08 13:54:22 -0600

    changed homepage description

--- hope-simpson-web-app/src/Home/Home.js
+++ hope-simpson-web-app/src/Home/Home.js
@@ -12,22 +12,22 @@ function Home() {
       <h1 className="text-center">Exploring COVID-19 Cases Around the World</h1>
       <h2 className="text-center">Modern Tools Applied to a 90’s Concept</h2>
       <hr/>
-      <p className="lead">A very cordial welcome to the home page for work done by some of the members of the fall class of COMP/ELEC 425/554! The class assignment was to create a platform based on modern databases, visualization, web design, virtual machines, signal processing, and data analysis. Then to use this platform to refresh past work, that studied the spread of the influenza virus, to our modern problem of COVID-19.</p>
-      <p className="lead">On our Home page, you can begin a bit of exploration of your own looking at cases around the world. Explore the map, click on a location, and see a plot of the daily reported cases and a histogram of the cases, on a weekly basis.</p>
+      <p className="lead">A very cordial welcome to the Hope-Simpson project by a group of students from the fall 2020 class of COMP/ELEC 425/554! We created a platform based on modern databases, visualization, web design, virtual machines, signal processing, and data analysis. We applied this platform to analyzing COVID-19 data based on work from a past study where a general practitioner, Robert Edgar Hope-Simpson, studied the spread of the influenza virus. We hope you find our website compelling and insightful!</p>
+      <p className="lead">On our home page, you can begin to explore COVID-19 cases around the world. The histogram displays confirmed cases in weekly increments based on the locations you selected.</p>
       <div style={{ height: 1050 }}>
         <TableauDashboard
           url={ TABLEAU_WORLD_MAP_DASHBOARD_URL }
           options={ TABLEAU_OPTIONS }
         />
       </div>
-      <p className="lead">Then let us take you through a journey of some other ways to look at a worldwide viral pandemic. Here is what you can find on our other pages:</p>
+      <p className="lead">Let us take you through a journey of ways to look at the current worldwide pandemic through the lens of Hope-Simpson. Here is what you can find on our other pages:</p>
       <ul>
-        <li className="lead"><strong>About:</strong> A few slides giving you some background on our inspiration which is the life work of Robert Edgar Hope-Simpson (1908-2003), a general practitioner who studied, for sixty years, influenza cases around the world. Then a bit about where we get our data, and some things it has begun to show us.</li>
-        <li className="lead"><strong>Map@Tableau:</strong> Using Tableau, a Hope-Simpson inspired look at COVID-19 by sampling regions around the world in four latitude zones, comparing and contrasting different regions, and showing a north-to-south longitudinal slice of the Americas.</li>
-        <li className="lead"><strong>Map@GDS:</strong> Using Google Data Studio, explore fine controls for exploring the data by date, country, latitude and longitude, see another view of the four-zones concept, and another view of the longitudinal slice of the Americas.</li>
-        <li className="lead"><strong>Modeling:</strong> See the early work of analysis that can be done on the data as we explore a compartmental network model for analysis of the virus spread and a Fourier-based analysis of the reported cases across the world.</li>
+        <li className="lead"><strong>About:</strong> Slides about the background inspiration, which is the life work of Robert Edgar Hope-Simpson (1908-2003), a general practitioner who studied influenza cases around the world for sixty years. Also, an explanation of the Hope-Simpson concept and about our COVID-19 data.</li>
+        <li className="lead"><strong>Map@Tableau:</strong> Using Tableau, you can take a Hope-Simpson inspired look at COVID-19 through a sample of four latitude zones around the world. We compared and contrasted different regions and showed a north-to-south longitudinal slice of the Americas.</li>
+        <li className="lead"><strong>Map@GDS:</strong> Using Google Data Studio, you can dive into a detailed exploration of the data by date, country, latitude and longitude. We can see another view of the four-zones concept and the longitudinal slice of the Americas.</li>
+        <li className="lead"><strong>Modeling:</strong> See early work of a compartmental network model of the virus spread and a Fourier-based analysis of the reported cases across the world.</li>
       </ul>
-      <p className="lead">Finally, our site is designed to let you download the data you see to do your own investigation.</p>
+      <p className="lead">Finally, our site is designed to let you download the data so you can do your own investigation.</p>
       <p className="lead">Stay safe!</p>
     </div>
   );
