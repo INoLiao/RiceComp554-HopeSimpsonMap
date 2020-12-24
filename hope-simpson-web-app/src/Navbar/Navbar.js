73dcaddd4584e1497f1535067cc30e567546a314
INoLiao <ino.liao@gmail.com>
2020-12-24 09:24:58 -0600

    fixed navbar expand adn collapse issue in small screens

--- hope-simpson-web-app/src/Navbar/Navbar.js
+++ hope-simpson-web-app/src/Navbar/Navbar.js
@@ -8,28 +8,28 @@ function Navbar() {
       <div className="container navbar-container">
         <div className="row-fluid">
           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content"
+                    aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon" />
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="collapse navbar-collapse" id="navbar-content">
               <ul className="navbar-nav">
-                <li className="nav-item">
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                   <NavLink exact className="nav-link" to="/">Home</NavLink>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                   <NavLink exact className="nav-link" to="/about">About</NavLink>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                   <NavLink exact className="nav-link" to="/map/tableau">Tableau Map<span className="description"></span></NavLink>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                   <NavLink exact className="nav-link" to="/map/gds">GDS Map<span className="description"></span></NavLink>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                   <NavLink exact className="nav-link" to="/modeling">Modeling</NavLink>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                   <NavLink exact className="nav-link" to="/team">Team</NavLink>
                 </li>
               </ul>
